53f68c783bc0263616f75288f617ae8ee4f72294
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2024-11-15 17:29:00 -0500

    add UI for grain duration

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -4,55 +4,70 @@ import GranularNode from 'wasm-audio-worklet';
 
 const app = document.querySelector<HTMLDivElement>('#app') as HTMLDivElement;
 
-function createInteractiveSVG(
+function createInteractive3DSVG(
     container: HTMLElement,
     width: number,
     height: number,
-    cb: (x: number, y: number) => void,
+    callback: (xPortion: number, yPortion: number, zPortion: number) => void
 ): void {
-
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("width", width.toString());
     svg.setAttribute("height", height.toString());
     svg.style.border = "1px solid black";
     svg.style.cursor = "pointer";
 
+    const minZ = 0.001; // Minimum z value (to avoid size being zero)
+    const maxZ = 1.0; // Maximum z value
+    let zPortion = 0.5; // Default z value
+
     const verticalLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    verticalLine.setAttribute("x1", "0");
-    verticalLine.setAttribute("y1", "0");
-    verticalLine.setAttribute("x2", "0");
-    verticalLine.setAttribute("y2", height.toString());
     verticalLine.setAttribute("stroke", "black");
     verticalLine.setAttribute("stroke-width", "2");
     svg.appendChild(verticalLine);
 
     const horizontalLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    horizontalLine.setAttribute("x1", "0");
-    horizontalLine.setAttribute("y1", "0");
-    horizontalLine.setAttribute("x2", width.toString());
-    horizontalLine.setAttribute("y2", "0");
     horizontalLine.setAttribute("stroke", "black");
     horizontalLine.setAttribute("stroke-width", "2");
     svg.appendChild(horizontalLine);
+
+    const positionCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    positionCircle.setAttribute("r", (zPortion * 50).toString());
+    positionCircle.setAttribute("fill", "red");
+    svg.appendChild(positionCircle);
 
     svg.addEventListener("mousemove", (event: MouseEvent) => {
         const rect = svg.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
         const mouseY = event.clientY - rect.top;
+
+        const xPortion = mouseX / width;
+        const yPortion = mouseY / height;
+
         verticalLine.setAttribute("x1", mouseX.toString());
         verticalLine.setAttribute("x2", mouseX.toString());
+        verticalLine.setAttribute("y1", "0");
+        verticalLine.setAttribute("y2", height.toString());
+
+        horizontalLine.setAttribute("x1", "0");
+        horizontalLine.setAttribute("x2", width.toString());
         horizontalLine.setAttribute("y1", mouseY.toString());
         horizontalLine.setAttribute("y2", mouseY.toString());
-        const x = mouseX/width;
-        const y = mouseY/height;
-        cb(x,1-y);
+
+        positionCircle.setAttribute("cx", mouseX.toString());
+        positionCircle.setAttribute("cy", mouseY.toString());
+
+        callback(xPortion, yPortion, zPortion);
+    });
+
+    svg.addEventListener("wheel", (event: WheelEvent) => {
+        event.preventDefault();
+        zPortion = Math.min(maxZ, Math.max(minZ, zPortion - event.deltaY * 0.001));
+        positionCircle.setAttribute("r", (zPortion * 50).toString());
+        callback(0, 0, zPortion); // Provide updated z value in the callback
     });
 
     container.appendChild(svg);
 }
-
-
-
 
 function initButton() {
     const button = document.createElement("button")
@@ -72,9 +87,10 @@ function initButton() {
         // @ts-ignore TODO: emit types from wasm-audio-worklet
         node.connect(ac.destination);
 
-        createInteractiveSVG(app, 300, 300, (x, y) => {
+        createInteractive3DSVG(app, 300, 300, (x, y, z) => {
             node.parameters.get('start').setValueAtTime(x, ac.currentTime);
             node.parameters.get('range').setValueAtTime(y*y*y, ac.currentTime);
+            node.parameters.get('grainDuration').setValueAtTime(z*z*z, ac.currentTime);
         });
     });
 
