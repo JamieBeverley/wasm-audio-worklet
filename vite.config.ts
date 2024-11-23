af22c7d1ee0e9ca2b97c506c74ee74f2345b8ab7
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2024-11-23 11:24:10 -0500

    remove unused vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
     plugins: [
-        wasm(),            // Allows importing .wasm files
-        topLevelAwait(),   // Enables using top-level await with .wasm modules if needed
         dts(), // TypeScript types
     ],
     build: {
@@ -26,5 +22,4 @@ export default defineConfig({
             '@': resolve(__dirname, 'src'), // Optional alias for cleaner imports
         },
     },
-    //   publicDir: 'rust-wasm/pkg', // Includes static assets from rust-wasm/pkg/*.wasm
-})
+});
