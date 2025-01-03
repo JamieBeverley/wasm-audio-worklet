705f83f1fd3387721988573fda4a99e7cbfa460f
Jamie Beverley <11593376+JamieBeverley@users.noreply.github.com>
2025-01-02 23:14:37 -0500

    refactor worklets, dedupe processor logic

--- public/worklets.js
+++ public/worklets.js
@@ -52,6 +52,7 @@ class WasmProcessor extends AudioWorkletProcessor {
             data.wasmBytes, imports
         )).instance.exports;
         this.port.postMessage({ type: "init-wasm-complete" });
+        this.alloc_memory();
     }
 
     messageHandlers = {
@@ -72,8 +73,7 @@ class WasmProcessor extends AudioWorkletProcessor {
     process(inputs, outputs, parameters) {
         if (
             this._wasm === null ||
-            (inputs[0][0] === undefined) ||
-            this.sampleBuffer == null
+            (inputs[0][0] === undefined)
         ) {
             return true;
         }
@@ -86,22 +86,30 @@ class WasmProcessor extends AudioWorkletProcessor {
 
 class BufferLooper extends WasmProcessor {
 
-    static get parameterDescriptors() {
-        return []
+    alloc_memory(bufferLength) {
+        // TODO a bit ugly, but changing alloc_memory to a no-op so that it can
+        // be done in initBuffer (because we need the sample buffer to alloc
+        // memory)
+        // Can probably be undone after figuring out proper memory management of
+        // buffers (why does the order matter?)
+        // Hypothesis: usually when we allocate the sample buffer the wasm
+        // memory needs to grow. When wasm memory grows, the previous buffers
+        // are transfered and become un-usable.
+        // SO: any time we (re)set the sample buffer, check if memory needs to
+        // grow, if it does, re-allocate the in/out buffers as well.
     }
 
-    alloc_memory(bufferLength) {
-        // Alloc memory here is a bit different: we need to alloc for the sample
-        // buffer too.
+    _alloc_memory(bufferLength){
         this.sampleBuffer = new WasmBuffer(bufferLength);
         this.sampleBuffer.alloc(this._wasm);
         this.inBuffer.alloc(this._wasm);
         this.outBuffer.alloc(this._wasm);
+
     }
 
     initBuffer(data){
         const { length, channelData } = data.data;
-        this.alloc_memory(length);
+        this._alloc_memory(length);
 
         this.sampleBuffer.buffer.set(channelData);
         this._wasm.synth_set_buffer(this.sampleBuffer.ptr, length)
@@ -112,108 +120,14 @@ class BufferLooper extends WasmProcessor {
         "init-wasm": this.initWasm,
         "init-buffer": this.initBuffer,
     }
+
+    process(inputs, outputs, parameters) {
+        if(this.sampleBuffer === null) return true
+        return super.process(inputs, outputs, parameters)
+    }
+
 }
 
 registerProcessor('BufferLooper', BufferLooper);
-
-// TODO dedupe/refactor/clean/figure out correct abstraction here
-
-class BitCrush extends AudioWorkletProcessor {
-
-    static get parameterDescriptors() {
-        return []
-    }
-
-    constructor() {
-        super();
-        this._wasm = null;
-
-        this._inPtr = null;
-        this._outPtr = null;
-
-        this._inBuf = null;
-        this._outBuf = null;
-
-        // this is default for WAPI - it will probably be variable some day and
-        // need to be accepted from a post-message?
-        this._block_size = 128;
-
-        this.port.onmessage = event => this.onmessage(event.data);
-    }
-
-    alloc_memory() {
-        // allocates the heap memory for rust, returns a pointer to it.
-        // NOTE: we may need/want to do this multiple times, once per
-        //      channel per in/out?
-        //      mem allocation  = n_channels * 2 * this._block_size.
-        //      Possibly useful to have a class for this (both the ptr 
-        //      and the Float32Array instead of having these separated).
-        this._inPtr = this._wasm.alloc_block();
-        this._outPtr = this._wasm.alloc_block();
-
-        this._inBuf = new Float32Array(
-            // but `memory` isn't exported from rust, where does it come from?
-            // ChatGPT says when you allocate dynamic memory in wasm, this 
-            // `memory` object gets defined on `exports`. (TODO validate? read?)
-            this._wasm.memory.buffer,
-            this._inPtr,
-            this._block_size
-        )
-        this._outBuf = new Float32Array(
-            this._wasm.memory.buffer,
-            this._outPtr,
-            this._block_size
-        );
-    }
-
-    onmessage(data) {
-        if (data.type === 'init-wasm') {
-            const instance = async () => {
-                const memory = new WebAssembly.Memory({
-                    // TODO totally arbitrary, perhaps parametrize in post
-                    // message or consider a reasonable default.
-                    initial: 3
-                });
-
-                const imports = {
-                    env: {
-                        memory: memory,
-                    }
-                };
-
-                this._wasm = (await WebAssembly.instantiate(
-                    data.wasmBytes, imports
-                )).instance.exports;
-
-                this.port.postMessage({ type: "init-wasm-complete" });
-            }
-            instance().then(() => this.alloc_memory());
-        }
-    }
-
-    process(inputs, outputs, parameters) {
-        if (
-            this._wasm === null || (inputs[0][0] === undefined) || this._inBuf === null
-        ) {
-            return true;
-        }
-
-        // 1. Copy input data to t`this._inPtr`
-        this._inBuf.set(inputs[0][0]) // array index may not be correct
-
-        // 2. call wasm process, passing pointers to the input/output and how many
-        // bytes to read/process
-        this._wasm.process(this._inPtr, this._outPtr, this._block_size);
-        // 3. copy the processed values back over to the worklet output array
-        // NOTE: unfortunately a `set` here (which copies values, not
-        // references) is unavoidable. Still probably faster than iterating over
-        // every sample.
-        outputs[0][0].set(this._outBuf)
-
-        return true;
-    }
-
-}
-
-registerProcessor('BitCrush', BitCrush);
+registerProcessor('BitCrush', WasmProcessor);
 
